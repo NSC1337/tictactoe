1e57b73e6f18bafa9873b9cb168036fa20c8eb7b
NSC1337 <nicholasscottcarroll@gmail.com>
2024-05-03 19:55:32 -0400

    fix

--- main.py
+++ main.py
@@ -11,28 +11,27 @@ board = [3 * [' '] for i in range(3)]
 
 # Returns 0 if nobody wins, 1 if X wins, 2 if O wins
 def winner():
-    tile_to_case = {' ': 0, 'X': 1, 'O':2}
     # Check cols
     for i in range(3):
         if board[i][0] == board[i][1] == board[i][2]:
-            return tile_to_case[board[i][0]]
+            return True
     # Check rows
     for row in board:
         if row[0] == row[1] == row[2]:
-            return tile_to_case[row[0]]
+            return True
     # Check diagonals
     if board[0][0] == board[1][1] == board[2][2]:
-        return tile_to_case[board[0][0]]
+        return True
     if board[2][1] == board[1][1] == board[0][2]:
-        return tile_to_case[board[2][1]]
-
+        return True
+    return False
 
 nmoves = 0
 while True:
     # make a move X
 
     # if win, exit X WIN
-    # if nmoves = 9, exit DRAW
-    # make a move y
+    # if nmoves = 9, exit DRAWi
+    # make a move 
     # if win, exit Y WIN
 
